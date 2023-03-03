769797929cddcc5a98d86532cde8981f116f400e
Ari Asulin <ari.asulin@gmail.com>
2023-03-03 12:26:19 -0500

    ImageAsset bugfix

--- src/components/asset-browser/client/asset-types/image/ImageAsset.js
+++ src/components/asset-browser/client/asset-types/image/ImageAsset.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import {getMarkdownOptions} from "../markdown/markdownOptions.js";
 import AssetBrowserContext from "../../context/AssetBrowserContext.js";
 import ErrorBoundary from "../../error/ErrorBoundary.js";
+import Markdown, {compiler} from "markdown-to-jsx";
 
 
 class ImageAsset extends React.Component {
@@ -69,15 +70,16 @@ class ImageAsset extends React.Component {
         return false;
     }
 
-    openInFullScreen() {
+    async openInFullScreen() {
         let {title, src, assetBrowser} = this.props;
-        const altContent = this.getAltContent();
+        const altContent = (await this.loadAltSource()) || this.getAltContent();
+        const altString = stripMarkup(altContent)
         const fullscreenContent = <img
             key={src}
             className="fullscreen-image"
             src={src}
-            alt={altContent}
-            title={title || altContent}
+            alt={altString}
+            title={title || altString.replace(/\n/g, " ")}
         />
         assetBrowser.showFullScreenAsset(this, fullscreenContent, src, altContent);
         this.ref.img.current.scrollIntoView({block: "start", behavior: 'smooth'})
@@ -102,13 +104,16 @@ class ImageAsset extends React.Component {
         let finalSrc = src;
         if (refreshHash && refreshHash !== this.state.originalRefreshHash)
             finalSrc += '?refreshHash=' + refreshHash;
+
+        const altString = stripMarkup(altContent)
+
         return <img
             {...extraProps}
             key="image"
             className={className}
             src={finalSrc}
-            alt={altContent}
-            title={title || altContent.replace(/\n/g, " ")}
+            alt={altString}
+            title={title || altString.replace(/\n/g, " ")}
             onClick={this.cb.onClick}
             ref={this.ref.img}
         />
@@ -117,18 +122,43 @@ class ImageAsset extends React.Component {
 
     async loadAltSource() {
         let {src, alt} = this.props;
-        const {altContent} = this.state;
-        if (!altContent && alt === true) {
-            let pos = src.lastIndexOf(".");
-            const altSrc = src.substring(0, pos < 0 ? src.length : pos) + ".md";
-            const promise = new Promise(async (resolve, reject) => {
-                const response = await fetch(altSrc);
-                const altContent = await response.text()
+        let {altContent} = this.state;
+        if (!altContent) {
+            if (alt === true) {
+                let pos = src.lastIndexOf(".");
+                const altSrc = src.substring(0, pos < 0 ? src.length : pos) + ".md";
+                altContent = new Promise(async (resolve, reject) => {
+                    const response = await fetch(altSrc);
+                    const altContent = await response.text()
+                    this.setState({altContent})
+                    resolve();
+                })
                 this.setState({altContent})
-            })
-            this.setState({altContent: promise})
+            }
         }
+        return altContent;
     }
+}
+
+
+function stripMarkup(markdownContent) {
+    const contentList = compiler(markdownContent, {wrapper: null});
+
+    function strip(contentList) {
+        let string = '';
+        for (const content of contentList) {
+            let contentString = content;
+            if (typeof content !== "string") {
+                if (typeof content !== "object" || !content.props.children)
+                    throw new Error("Invalid object");
+                contentString = strip(content.props.children)
+            }
+            string += (string ? " " : "") + contentString.trim();
+        }
+        return string.trim();
+    }
+
+    return strip(contentList);
 }
 
 
